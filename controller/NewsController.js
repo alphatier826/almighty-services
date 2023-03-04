8930bcc95e9f646ea5cb6be8870043d50a162a23
Ankur9900 <arya.ankur99@gmail.com>
2023-03-04 23:05:43 +0530

    router change

--- controller/NewsController.js
+++ controller/NewsController.js
@@ -2,6 +2,6 @@ const express = require('express');
 const NewsController = express.Router();
 const {NewsService} = require("./../service/NewsService");
 
-NewsController.get("", (req, res) => returnResponse(req, res, NewsService.getAll()));
+NewsController.get("/", (req, res) => returnResponse(req, res, NewsService.getAll()));
 
 module.exports = {NewsController};
